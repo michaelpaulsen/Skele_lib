1b15911f9683364f9be684031f905d206998dc35
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-06-26 20:51:21 -0700

    fixed some safty issues
    fixed some safty issues
    but having some issues with overflowing

--- utils/Array.h
+++ utils/Array.h
@@ -18,24 +18,36 @@ namespace Skele_lib {
 			~Array()
 			{
 				free(this->data);
+				this->data = nullptr; 
+				this->items = NULL;
+				this->length = NULL;
 			}
 
 			void Push(t newLast) {
-				if (this->items == this->length) {
-					this->length *= 2;
-					this->data = static_cast<t*>(realloc(this->data, sizeof(t) * length));
-					assert(this->data != nullptr);
+				if(this->data != nullptr){ // the array has not been distroyed 
+					if (this->items == this->length) {
+						this->length *= 2;
+						this->data = static_cast<t*>(realloc(this->data, sizeof(t) * length)); // &this->data will be the same unless there's not enogh room at &this->data to realloc to the new size
+						assert(this->data != nullptr);// make shure that it actualy worked 
+						/*get more mem for the array if the size is too small to store the next value*/
+					}
+					this->items++;//add one to the items "index"
+					this->data[this->items] = newLast;
 				}
-				this->items++;
-				this->data[this->items] = newLast;
 			}
 			t Pop() {
-				t temp = data[this->items];
-				this->items--;
-				return temp;
+				if (this->data != nullptr || this->items <=0) {
+					return NULL;
+				} // return NULL if this has already been distroyed or the array has no more items in it 
+					t temp = data[this->items];
+					this->items--;
+					return temp;// otherwise return the value of the last item in the array
 			}
 			t operator [](int i) {
-				return this->data[i];
+				return this->data != nullptr ?this->data[i]:NULL;// safely index the array
+			}
+			int Length(void) {
+				return this->items;
 			}
 		};
 	}
