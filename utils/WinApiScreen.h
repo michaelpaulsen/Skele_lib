673ef5f8b3956489c87bcdd073041e41f81be409
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-07-06 18:13:22 -0700

    Create WinApiScreen.h
    a class that encapulates the Windows API screen setup

---
+++ utils/WinApiScreen.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <thread>
+#include <vector>
+#include <stdio.h>
+#include <Windows.h>
+#include <conio.h>
+namespace Skele_lib {
+	namespace CustomWinAPI{
+		namespace screen {
+			class Screen {
+				DWORD dwBytesWritten = 0;
+				wchar_t* screen;
+				HANDLE hConsole; 
+				int w, h;
+			public:
+				Screen(int w, int h) {
+					hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+					SetConsoleActiveScreenBuffer(this->hConsole);
+					screen = new wchar_t[w * h];
+					this->w = w;
+					this->h = h;
+				}
+				void setActive() {
+					SetConsoleActiveScreenBuffer(this->hConsole);
+				}
+				void writeScreen() {
+					WriteConsoleOutputCharacter(this->hConsole, this->screen, this->h * this->w, { 0,0 }, &this->dwBytesWritten);
+				}
+				
+			};
+		}
+	}
+}
