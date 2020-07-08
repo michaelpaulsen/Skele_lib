535695d76f7df8bd8217940dc0e4c0e9fac7406a
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-07-07 19:37:21 -0700

    added a file to include all files in one line

---
+++ IncludeAll.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../Skele_lib/Duration.h"
+#include "../Skele_lib/math.h"
+#include "../Skele_lib/ShapeLib.h"
+#include "../Skele_lib/SpeedRun.h"
+#include "../Skele_lib/V2d.h"
+#include "../Skele_lib/RPG_Lib/skele_RPG_Lib.h"
+#include "../Skele_lib/utils/Array.h"
+#include "../Skele_lib/utils/Utils.h"
