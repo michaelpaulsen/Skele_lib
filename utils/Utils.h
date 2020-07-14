849a868ba1513d26eb892f7935fd85de008ab58f
michaelpaulsen <michael.j.paulsen@gmail.com>
2020-07-13 17:08:05 -0700

    added isPrintfSafe function
    isPrintfSafe checks to see if the passed in char* is safe to print with printf

--- utils/Utils.h
+++ utils/Utils.h
@@ -18,5 +18,28 @@ namespace Skele_lib {
 		char longToChar(long x){ /// returns the LSD of the passed in int as a char
 			return (x % 10) + 48;
 		}
+		bool isPrintfSafe(const char* s) {
+			int i = 0; 
+			while(s[i] != '\0') {
+				if (s[i] == '%') { 
+				/* 
+				* doing it this way prevents it from having
+				* to do the check for the second char if the first char isn't a %
+				* in theory this is faster though I wouldn't be suprised if the compiler is sees this as
+				* the same as having two seperate if statments 
+				* I also like this better because it repersents the flow of the algorithom better
+				*/
+					if (s[i + 1] == '%') {
+						i += 2;
+						continue;
+					}
+					if (s[i + 1] != '%') {
+						return false;
+					}
+				}
+				i++;
+			}
+			return true; 
+		}
 	}
 }
