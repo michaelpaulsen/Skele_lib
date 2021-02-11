b6316bb29850b3e270ffb70300b1126af11244fb
michaelpaulsen <michael.j.paulsen@gmail.com>
2021-02-10 20:07:47 -0800

    added a SetScreenSize function that sets the screen size
    ++void SetScreenSize(short w, short h)

--- utils/WinApiScreen.h
+++ utils/WinApiScreen.h
@@ -1,21 +1,21 @@
 #pragma once
 #include <vector>
 #include <Windows.h>
+#include <WinUser.h>
 namespace Skele_lib {
 	namespace CustomWinAPI{
 		namespace screen {
 			class Screen {
 				DWORD dwBytesWritten = 0;
 				HANDLE hConsole;
+				HWND Console;
 				int w, h;
 			public:
 				wchar_t* screen;
-				Screen(int w, int h) {
-					hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-					SetConsoleActiveScreenBuffer(this->hConsole);
-					screen = new wchar_t[w * h];
-					this->w = w;
-					this->h = h;
+								
+				void SetScreenSize(short w, short h) {
+					SetConsoleScreenBufferSize(this->hConsole, { w,h });
+					MoveWindow(this->Console, 0, 0, w, h, true);
 				}
 				void setActive() {
 					SetConsoleActiveScreenBuffer(this->hConsole);
@@ -28,6 +28,15 @@ namespace Skele_lib {
 				}
 				int getScreenHeight() {
 					return this->h;
+				}
+				Screen(int w, int h) {
+					Console = GetConsoleWindow();
+					hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+					SetScreenSize(w, h);
+					SetConsoleActiveScreenBuffer(this->hConsole);
+					screen = new wchar_t[w * h];
+					this->w = w;
+					this->h = h;
 				}
 			};
 		}
