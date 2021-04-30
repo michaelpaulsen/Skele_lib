f3847a7688429871e7430f75a2b31f70199cd565
michaelpaulsen <michael.j.paulsen@gmail.com>
2021-04-29 22:30:10 -0700

    added PHI constant
    ++ PHI = 1.61803398874989484820

--- math.h
+++ math.h
@@ -3,5 +3,6 @@ namespace Skele_lib{
 	namespace Math{
 		const long double PI = 3.14159265358979323846;
 		const long double RAD_TO_DEG = 180 / PI; 
+		const long double PHI = 1.61803398874989484820;
 	}
 }
