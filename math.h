ce69fc4346c0bb7b02adf93ab1f9f18d09dfe619
michaelpaulsen <michael.j.paulsen@gmail.com>
2021-07-07 11:05:35 -0700

    added quadratic_equation to math.h
     using the quadratic formula this function gives the values for x where y == 0
    the pramiters are as follows ax^2+bx+c
     the first long double in the pair is the "positve" one
    the second long double in the pair is the "negitive"

--- math.h
+++ math.h
@@ -1,8 +1,22 @@
 #pragma once; 
 namespace Skele_lib{
 	namespace Math{
-		const long double PI = 3.14159265358979323846;
-		const long double RAD_TO_DEG = 180 / PI; 
-		const long double PHI = 1.61803398874989484820;
+#include <utility>
+#include <math.h>
+
+namespace Skele_lib::Math{
+	const long double PI = 3.14159265358979323846;
+	const long double RAD_TO_DEG = 180 / PI; 
+	const long double PHI = 1.61803398874989484820;
+	std::pair<long double,long double> quadratic_equation(double a,double b,double c ){ 
+		/**
+		* using the quadratic formula this function gives the values for x where y == 0 
+		* the pramiters are as follows ax^2+bx+c 
+		* the first long double in the pair is the "positve" one 
+		* the second long double in the pair is the "negitive" 
+		*/
+		std::pair<long double, long double> temp; 
+		temp.first  = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+		temp.second = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
 	}
 }
