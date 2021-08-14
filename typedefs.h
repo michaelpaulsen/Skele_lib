81e707437b73f806a26b3bf6d48b96b270635e38
michaelpaulsen <michael.j.paulsen@gmail.com>
2021-08-14 02:23:31 -0700

    removed the V2d typedefs from typedefs,h
    Moved them into their own defining file to make it easier to metaprogram the Game engine ( that way I can know that they are defined  just by checking for the define in the v2d.h file)

--- typedefs.h
+++ typedefs.h
@@ -1,10 +1,4 @@
 #pragma once
-#ifdef vector2d
-typedef	Skele_lib::Math::V2d<int>       V2dI;
-typedef Skele_lib::Math::V2d<float>     V2dF;
-typedef Skele_lib::Math::V2d<double>    V2dD;
-#endif ///def vector2d
-#ifdef Array_Loaded
 typedef Skele_lib::Utils::Array<int>    ArrayI;
 typedef Skele_lib::Utils::Array<float>  ArrayF;
 typedef Skele_lib::Utils::Array<double> ArrayD;
