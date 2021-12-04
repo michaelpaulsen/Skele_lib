6786d631aef24225a489b7894374a946f29a2395
michaelpaulsen <michael.j.paulsen@gmail.com>
2021-12-03 16:35:31 -0800

    moved the physics files to their own file and started a rewrite of the rigid body class
    upon thinking about it I've decided that the rigid body class doesn't own the sprite that is displayed by it. the sprite should be owned by a child GameObject class. That is the class that you should inherit from. I was also adding support for my vector2D class witch cased the code to be hard to read and for there to be a lot of repeated code. also moved the enums that I had to their own file.. this allows the code to be cleaner and allows for better readability.

---
+++ RPG_Lib/Physics/Physics.hpp
@@ -0,0 +1,32 @@
+#include "PhysicsTypes.hpp"
+namespace Skele_lib {
+	namespace GameEngine {
+		namespace Physics {
+			class RigidBody {
+			private:
+				Physics_rect rect;
+				movable objectSpeed = { 0,0,0,0 };
+				movable drag = { 2,0,0,0 };
+			public:
+				RigidBody(int x, int y, float w, float h) {
+					rect = { x,y,w,h }; 
+				}
+				Physics_rect getPosition() {
+					return this->rect;
+				}
+				void addForce(double xcomp, double ycomp) {
+					objectSpeed.accx = xcomp;
+					objectSpeed.accy = ycomp; 
+				}
+				virtual void tick(std::chrono::duration<float> FelapsedTime, int h, int w) {
+					this->objectSpeed.velx += this->objectSpeed.accx--;
+					this->objectSpeed.velx -= this->drag.velx;
+					this->rect.x += this->objectSpeed.velx - this->drag.velx;
+				}
+				void operator()() {
+					this->tick(); 
+				}
+			};
+		}
+	}
+}
---
+++ RPG_Lib/Physics/PhysicsTypes.hpp
@@ -0,0 +1,27 @@
+namespace Skele_lib{
+	namespace GameEngine {
+		namespace Physics {
+			enum GravityDirection {
+				UP = 1,
+				DOWN = 3,
+				RIGHT = 2,
+				LEFT = 4
+			};
+			enum CollisionType {
+				BOUNCE = 1,
+				REFLECT = 2,
+				PUSH = 3,
+				DAMPEN = 4,
+				INTERACT = 5
+			};
+			struct Physics_rect {
+				int x, y;
+				float w, h;
+			};
+			struct movable {
+				double velx, vely, accx, accy;  
+			};
+		}
+	}
+}	
+
