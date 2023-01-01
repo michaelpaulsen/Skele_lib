01edba7432df030b3362580dc0d8b3c8090e2ea8
michaelpaulsen <michael.j.paulsen@gmail.com>
2023-01-01 05:21:57 -0800

    reimplemented Console using vfprintf
    vfprintf is a form of fprintf that instead of being variadic it takes in a va_list. this allows you to make your functions "printf like" where that means that it takes in a format string and any number of vars then outputs them in the same way as printf would. This greatly simplified this API

--- skele_Console_lib/SK_ConsoleWIN.h
+++ skele_Console_lib/SK_ConsoleWIN.h
@@ -1,69 +1,87 @@
 #ifndef SK_CONSOLE_CLASS_WIN 
 #define SK_CONSOLE_CLASS_WIN 
 #include "./SK_ConsoleBase.h"
+#include <stdarg.h>
 #include <windows.h>
 
 namespace Skele_lib {
 	namespace Console {
-		struct Console : public ConsoleBase {
-			Console(FILE* _is = stdin, FILE* _os = stdout) {
+		class Console : public ConsoleBase {
+			HANDLE hConsole;
+			int printformmated(int attr, const char* fmnt, va_list valist){
+				short x;
+				GetColor(x);
+				SetConsoleTextAttribute(this->hConsole, attr);
+				auto ret_val = vfprintf(os, fmnt, valist);
+				SetConsoleTextAttribute(this->hConsole, x);
+				return ret_val;
+			}
+		public:
+			Console(FILE* _os = stdout) {
 				os = _os;
-				is = _is;
 				this->ok_color = Skele_lib::Console::Color(14, 02);
 				this->inform_color = Skele_lib::Console::Color(11, 01);
 				this->warn_color = Skele_lib::Console::Color(07, 06);
 				this->error_color = Skele_lib::Console::Color(07, 04);
 				hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 			}
-			HANDLE hConsole;
+			int Log(const char* fmnt, ...)override {
+				int c = (16 * inform_color.bgc) + error_color.fgc;
+				va_list valist;
+				va_start(valist, fmnt);
+				auto retval = printformmated(c, fmnt, valist);
+				va_end(valist);
+				return retval;
+
+			};
+
+			int Warn(const char* fmnt, ...)override {
+				int c = (16 * warn_color.bgc) + error_color.fgc;
+				va_list valist;
+				va_start(valist, fmnt);
+				auto retval = printformmated(c, fmnt, valist);
+				va_end(valist);
+				return retval;
+			};
+
+			int OK(const char* fmnt, ...)override {
+				int c = (16 * ok_color.bgc) + error_color.fgc;
+				va_list valist;
+				va_start(valist, fmnt);
+				auto retval = printformmated(c, fmnt, valist);
+				va_end(valist);
+				return retval;
+			};
+
+			int Error(const char* fmnt, ...)override {
+				int c = (16 * error_color.bgc) + error_color.fgc;
+				va_list valist;
+				va_start(valist, fmnt);
+				auto retval = printformmated(c, fmnt, valist);
+				va_end(valist);
+				return retval;
+			};
+
+			int Print(const char* fmnt, int fgc, int bgc, ...)override {
+				int c = (16 * bgc) + fgc;
+				va_list valist;
+				va_start(valist, fmnt);
+				auto retval = printformmated(c, fmnt, valist);
+				va_end(valist);
+				return retval;
+			};
+			void Setattr(short attr, short& oldval) {
+				GetColor(oldval);
+				SetConsoleTextAttribute(this->hConsole, attr);
+			};
 			bool GetColor(short &ret) {
 				CONSOLE_SCREEN_BUFFER_INFO info;
 				if (!GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info))return false;
 				ret = info.wAttributes;
 				return true;
 			}
-			HANDLE GetHandle() {
-				return this->hConsole;
-			}
-			int PrintColor(const char* print_me, int color, int BG_color = 0) override{
-				int c = (16 * BG_color) + color;
-				short x;
-				this->GetColor(x);
-				SetConsoleTextAttribute(this->hConsole, c);
-				auto ret_val = fprintf(this->os, "%s", print_me);
-				SetConsoleTextAttribute(this->hConsole, x);
-				return ret_val;
-			}			
-			int PrintColor(const float print_me, int color, int BG_color = 0) override{
-				int c = (16 * BG_color) + color;
-				short x;
-				this->GetColor(x);
-				SetConsoleTextAttribute(this->hConsole, c);
-				int ret_val;
-				if (print_me - static_cast<int>(print_me) == 0) {
-					ret_val = fprintf(this->os, "%d", static_cast<int>(print_me));
-				}
-				else {
-					ret_val = fprintf(this->os, "%f", print_me);
-				}
-				SetConsoleTextAttribute(this->hConsole, x);
-				return ret_val;
-			}
-			int PrintNumebrAsHex(long print_me)override {
-				return ret_val = fprintf(this->os, "%08x ", print_me & 0xff);
-			}
-			int PrintCharAsHex(char print_me) override {
-				int c = 0x02;
-				if (!print_me) c = 0x04;
-				short x;
-				this->GetColor(x);
-				SetConsoleTextAttribute(this->hConsole, c);
-				auto ret_val = fprintf(this->os, "%02x ", print_me&0xff);
-				SetConsoleTextAttribute(this->hConsole, x);
-				return ret_val;
-			};
-			
 		};
+			
 	}
 }
 #endif // SK_CONSOLE_CLASS_WIN 
