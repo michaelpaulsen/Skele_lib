fc9ddd2cb1a3f2dcd721184ffa5798f0f18c186c
michaelpaulsen <michael.j.paulsen@gmail.com>
2023-01-02 17:16:36 -0800

    added vprintEx & printEx to unix console API
    Print Ex is an extended version of Print that allows you to set more than just the color of the text. this only works on UNIX (tested on ubuntu)  as of now because I cannot see any reason to how win32's SetConsoleAttributes function works.

--- skele_Console_lib/SK_ConsoleUNIX.h
+++ skele_Console_lib/SK_ConsoleUNIX.h
@@ -10,7 +10,18 @@ namespace Skele_lib {
 		class Console : public ConsoleBase {
 			const char color_code_start[5] = "\033[0;";
 			const char reset_code[5] = "\033[0m";
+			typedef char PRINTEX_FLAGS;
+			
 		public:
+			const static PRINTEX_FLAGS PRINTEX_BLINK           = 0b00000001;
+			const static PRINTEX_FLAGS PRINTEX_STRIKETHOUGH    = 0b00000010;
+			const static PRINTEX_FLAGS PRINTEX_OVERLINE        = 0b00000100;
+			const static PRINTEX_FLAGS PRINTEX_THICK_OVERLINE  = 0b00001000;
+			const static PRINTEX_FLAGS PRINTEX_UNDERLINE       = 0b00010000;
+			const static PRINTEX_FLAGS PRINTEX_THICK_UNDERLINE = 0b00100000;
+			const static PRINTEX_FLAGS PRINTEX_BRIGHT          = 0b01000000;
+			const static PRINTEX_FLAGS PRINTEX_DARK            = 0b10000000;
+
 			Console(FILE* _os = stdout) {
 				os = _os;
 				this->warn_color =  Color((int)FG_colors::YELLOW,(int)BG_colors::BLACK);
@@ -59,6 +70,29 @@ namespace Skele_lib {
 				va_end(args);
 				return ret_val;
 			};
+			int vPrintEX(PRINTEX_FLAGS flags, int fgc, int bgc, const char* msg, va_list valist) {
+				int retval = fprintf(os, "%s", color_code_start);
+				bool br = flags & PRINTEX_BRIGHT;
+				auto rbgc = bgc;
+				auto rfgc = fgc;
+				if (flags & PRINTEX_BLINK) { retval += fprintf(os, ";%d", 06); }
+				if (flags & PRINTEX_UNDERLINE) { retval += fprintf(os, ";%d", 04); }
+				if (flags & PRINTEX_THICK_UNDERLINE) { retval += fprintf(os, ";%d", 21); }
+				if (flags & PRINTEX_OVERLINE) { retval += fprintf(os, ";%d", 53); }
+				if (flags & PRINTEX_THICK_OVERLINE) { retval += fprintf(os, ";%d", 27); }
+				if (flags & PRINTEX_STRIKETHOUGH) { retval += fprintf(os, ";%d", 27); }
+				if (flags & PRINTEX_DARK) { retval += fprintf(os, ";%d", 02); }
+				if (fgc < 39 && br) { rfgc += 60; }
+				if (bgc < 49 && br) { rbgc += 60; }
+				retval += fprintf(os, "%d;%dm", rfgc, rbgc);
+				retval += vfprintf(os, msg, valist);
+				return retval;
+			}
+			int PrintEX(PRINTEX_FLAGS flags, int fgc, int bgc, const char* fmnt, ...) {
+				va_list valist; 
+				va_start(valist, fmnt); 
+				return vPrintEX(flags, fgc, bgc, fmnt, valist);
+			}
 			int EndLine() {
 				return fprintf(os, "%s\n", reset_code);
 			}
