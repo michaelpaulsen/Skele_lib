c660153f747e8c0b00c78b658903bec371462ed4
michaelpaulsen <michael.j.paulsen@gmail.com>
2023-01-02 18:15:26 -0800

    added wcscmpcs
    this takles a unicode string and converts it to a ascii string

--- utils/string_stuff.h
+++ utils/string_stuff.h
@@ -13,6 +13,20 @@ namespace Skele_lib {
     				};
     				return days[(dayindex - sub1)];
 			}
+			size_t wcscmpcs(const PWCHAR wide, const c_str thin) {
+				auto nob = (wcslen(wide) > strlen(thin) ? wcslen(wide) : strlen(thin))+1; 
+				PWCHAR wb = (PWCHAR)malloc(nob * sizeof(WCHAR));
+				c_str  tb = (c_str)malloc(nob); 
+				wcscpy_s(wb, nob, wide);
+				strcpy_s(tb, nob, thin); 
+				size_t i; 
+				for (size_t i = 0; i < nob; i++){
+					if (wb[i] != (short)tb[i]) {
+						return wb[i] - (short)tb[i];
+					}
+				}
+				return 0; 
+			}
 		}
 	}
 }
