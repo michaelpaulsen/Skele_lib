d4f0cc02be0c6c067543d6ca1750023b24fc88c3
michaelpaulsen <michael.j.paulsen@gmail.com>
2023-09-21 15:34:48 -0700

    consted all of the Console methods

--- console/Console.hpp
+++ console/Console.hpp
@@ -10,101 +10,101 @@ namespace SKC::Console {
 	public:
 		Console() = default;
 		~Console() = default;
-		void       Ok() {}
-		void     Warn() {}
-		void     Okln() {}
-		void    Error() {}
-		void    Print() {}
-		void   Inform() {}
-		void   Warnln() {}
-		void  Errorln() {}
-		void  Println() {}
-		void Informln() {}
+		void       Ok()const {}
+		void     Warn()const {}
+		void     Okln()const {}
+		void    Error()const {}
+		void    Print()const {}
+		void   Inform()const {}
+		void   Warnln()const {}
+		void  Errorln()const {}
+		void  Println()const {}
+		void Informln()const {}
 
 
-		void SetFGColor(int color);
-		void SetBGColor(int color);
-		void SetFGColor(int r, int g, int b);
-		void SetBGColor(int r, int g, int b);
-		void Reset(); 
-		void Blink(); 
+		void SetFGColor(int color) const;
+		void SetBGColor(int color) const;
+		void SetFGColor(int r, int g, int b) const;
+		void SetBGColor(int r, int g, int b) const;
+		void Reset() const;
+		void Blink() const;
 		
-		auto SetFGColor_s(int color);
-		auto SetBGColor_s(int color);
-		auto SetFGColor_s(int r, int g, int b);
-		auto SetBGColor_s(int r, int g, int b);
-		auto Reset_s();
-		auto Blink_s(); 
+		auto SetFGColor_s(int color) const;
+		auto SetBGColor_s(int color) const;
+		auto SetFGColor_s(int r, int g, int b) const;
+		auto SetBGColor_s(int r, int g, int b) const;
+		auto Reset_s() const;
+		auto Blink_s() const;
 
-		void Clear(); 
-		void Hide(); 
-		void Move(int x, int y); 
-		SKC_consoleVA void Print(printType msg1, printTypes... msg2);
-		SKC_consoleVA void Println(printType msg1, printTypes... msg2);
-		SKC_consoleVA void Ok(printType msg, printTypes ...msgs);
-		SKC_consoleVA void Inform(printType msg, printTypes ...msgs);
-		SKC_consoleVA void Warn(printType msg, printTypes ...msgs);
-		SKC_consoleVA void Error(printType msg, printTypes ...msgs);
-		SKC_consoleVA void Okln(printType msg, printTypes ...msgs);
-		SKC_consoleVA void Informln(printType msg, printTypes ...msgs);
-		SKC_consoleVA void Warnln (printType msg, printTypes ...msgs);
-		SKC_consoleVA void Errorln(printType msg, printTypes ...msgs);
-		SKC_consoleVA void ErrorAndDie(int exitcode, printType msg, printTypes ...msgs);
+		void Clear() const;
+		void Hide() const;
+		void Move(int x, int y) const;
+		SKC_consoleVA void Print (printType msg1, printTypes... msg2) const;
+		SKC_consoleVA void Println(printType msg1, printTypes... msg2) const;
+		SKC_consoleVA void Ok(printType msg, printTypes ...msgs) const;
+		SKC_consoleVA void Inform(printType msg, printTypes ...msgs) const;
+		SKC_consoleVA void Warn(printType msg, printTypes ...msgs) const;
+		SKC_consoleVA void Error(printType msg, printTypes ...msgs) const;
+		SKC_consoleVA void Okln(printType msg, printTypes ...msgs) const;
+		SKC_consoleVA void Informln(printType msg, printTypes ...msgs) const;
+		SKC_consoleVA void Warnln (printType msg, printTypes ...msgs) const;
+		SKC_consoleVA void Errorln(printType msg, printTypes ...msgs) const;
+		SKC_consoleVA void ErrorAndDie(int exitcode, printType msg, printTypes ...msgs) const;
 	private:
 		static inline char esc = 27; 
 	};
 	
-	void Console::SetFGColor(int color) {
+	void Console::SetFGColor(int color) const {
 		printf("%c[38;5;%dm", esc, color); 
 	};
-	void Console::SetFGColor(int r, int g, int b) {
+	void Console::SetFGColor(int r, int g, int b) const {
 		printf("%c[38;2;%d;%d;%dm", esc, r, g, b);
 
 	}
-	void Console::SetBGColor(int color) {
+	void Console::SetBGColor(int color) const {
 		printf("%c[48;5;%dm", esc, color);
 
 
 	};
-	void Console::SetBGColor(int r, int g, int b) {
+	void Console::SetBGColor(int r, int g, int b) const {
 		printf("%c[48;2;%d;%d;%dm", esc, r, g, b);
 
 	}
 	
-	void Console::Reset() {
+	void Console::Reset() const {
 		printf("%c[%cm", esc, 0);
 	}
-	void Console::Blink() {
+	void Console::Blink() const {
 		printf("%c[%dm", esc, 5);
 	}
 
-	auto Console::SetFGColor_s(int color) {
+	auto Console::SetFGColor_s(int color) const {
 		std::stringstream str;
 		str << esc << "[38;5;" << color << 'm';
 		return str.str(); 
 	};
-	auto Console::SetBGColor_s(int color) {
+	auto Console::SetBGColor_s(int color) const {
 		std::stringstream str;
 		str << esc << "[48;5;" << color << 'm';
 		return str.str();
 	};
-	auto Console::SetFGColor_s(int r, int g, int b) {
+	auto Console::SetFGColor_s(int r, int g, int b) const {
 		//printf("%c[38;2;%d;%d;%dm", esc, r, g, b);
 		std::stringstream str;
 		str << esc << "[38;2;" << r << ';'<< g << ';'<< b << 'm';
 		return str.str();
 	};
-	auto Console::SetBGColor_s(int r, int g, int b) {
+	auto Console::SetBGColor_s(int r, int g, int b) const {
 		std::stringstream str;
 		str << esc << "[48;2;" << r << ';' << g << ';' << b << 'm';
 		return str.str();
 	 };
-	auto Console::Reset_s() {
+	auto Console::Reset_s() const {
 		std::stringstream str;
 		str << esc << "["<< 0 << 'm';
 		return str.str();
 	 };
-	auto Console::Blink_s() {
+	auto Console::Blink_s() const {
 		std::stringstream str;
 		str << esc << "[" << 5 << 'm';
 		return str.str();
@@ -112,64 +112,64 @@ namespace SKC::Console {
 	}
 
 	
-	void Console::Hide() {
+	void Console::Hide()const  {
 		printf("%c[%cm", esc,8);
 	}
-	void Console::Clear() {
+	void Console::Clear() const {
 		printf("%cc",esc); 
 	}
-	void Console::Move(int x, int y) {
+	void Console::Move(int x, int y) const {
 		printf("%c[%d;%dH", esc, x, y);
 	}
-	SKC_consoleVA void Console::Print(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Print(printType msg1, printTypes... msg2)const{
 		std::cout << msg1;
 		Print(msg2...);
 	}
-	SKC_consoleVA void Console::Ok(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Ok(printType msg1, printTypes... msg2) const {
 		SetFGColor(0,255,0); 
 		SetBGColor(25,100,25);
 		Print(msg1, msg2...);
 	}
-	SKC_consoleVA void Console::Inform(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Inform(printType msg1, printTypes... msg2) const {
 		SetBGColor(100,100,255); 
 		SetFGColor(0,0,200);
 		Print(msg1, msg2...);
 	}
-	SKC_consoleVA void Console::Warn(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Warn(printType msg1, printTypes... msg2) const {
 		SetBGColor(200,100,0); 
 		SetFGColor(100, 50, 0);
 		Print(msg1, msg2...);
 	}
-	SKC_consoleVA void Console::Error(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Error(printType msg1, printTypes... msg2) const {
 		SetBGColor(255,100,100); 
 		SetFGColor(255, 0, 0);
 		Print(msg1, msg2...);
 	}
 
-	SKC_consoleVA void Console::Println(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Println(printType msg1, printTypes... msg2) const {
 		Print(msg1, msg2..., '\n');
 	}
-	SKC_consoleVA void Console::Okln(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Okln(printType msg1, printTypes... msg2) const {
 		Ok(msg1, msg2...);
 		Reset();
 		Print('\n');
 	}
-	SKC_consoleVA void Console::Informln (printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Informln (printType msg1, printTypes... msg2) const {
 		Inform(msg1, msg2...);
 		Reset();
 		Print('\n');
 	}
-	SKC_consoleVA void Console::Warnln(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Warnln(printType msg1, printTypes... msg2) const {
 		Warn(msg1, msg2...);
 		Reset();
 		Print('\n');
 	}
-	SKC_consoleVA void Console::Errorln(printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::Errorln(printType msg1, printTypes... msg2) const {
 		Error(msg1, msg2...);
 		Reset();
 		Print('\n');
 	}
-	SKC_consoleVA void Console::ErrorAndDie(int exitcode, printType msg1, printTypes... msg2) {
+	SKC_consoleVA void Console::ErrorAndDie(int exitcode, printType msg1, printTypes... msg2) const {
 		Error(msg1,msg2...);
 		Reset();
 		exit(exitcode);
